4f7315a477f29d14d8f84c5b34e649ce7d12ee91
Tank <kazimer@users.noreply.github.com>
2020-01-21 14:07:42 -0500

    Added a revert file because am lazy

---
+++ change_revert.ps1
@@ -0,0 +1,9 @@
+$oldName = 'Changed'
+$newName = 'Administrator'
+$user = Get-WMIObject Win32_UserAccount -Filter "Name='$oldName'"
+$result = $user.Rename($newName)
+
+if ($result.ReturnValue -eq 0) {
+return $user
+return $result
+}
